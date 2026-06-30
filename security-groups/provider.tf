7071af9c141557f46287ddd806d897a9e4e25ce4
megha <megha@gmail.com>
2026-07-01 02:54:40 +0530

    01-07

---
+++ security-groups/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.52.0"
+    }
+  }
+  backend "s3" {
+    bucket = "hjbhjbwehjbe"
+    region = "us-east-1"
+    key = "01-07-STATEFILE-SG"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
