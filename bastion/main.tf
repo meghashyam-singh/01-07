c4bcd731036e7d07c9fae9565820bd01a24c5bc2
megha <megha@gmail.com>
2026-07-02 02:11:13 +0530

    01-07

---
+++ bastion/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "bastion" {
+    ami = data.aws_ami.ami_id.id
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
+    subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
+    vpc_security_group_ids = [ data.aws_ssm_parameter.bastion_security_group_id.value ]
+    tags = {
+        Name = "${local.common_name}_bastion"
+    }
+}
